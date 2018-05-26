7bb93666f66335705b87a0520a7579591dc3cf80
Amin Adel <amin.adel@hotmail.com>
2018-05-26 15:57:02 +0430

    first uplaod

---
+++ src/js/gradient.js
@@ -0,0 +1,16 @@
+//---> Create	:	1397.03.05
+//---> Finish	:	--
+//---> Update	:	--
+/*****************************/
+$(document).ready(function () {
+	
+	if ($('.bg_gradient').length === 0) return;
+	
+	// usage example :
+	// <div class="bg_gradient" data-gradient="to bottom left, red_100, red_300"></div>
+	
+	var gradient_items = $('.bg_gradient');
+	$(gradient_items).each(function(key, item) {
+		
+	});
+});
